d6f8adf26a11287d88580ac2fa7a9425c28fc6dd
fercho-099 <69776793+fercho-099@users.noreply.github.com>
2024-05-11 22:49:51 -0300

    22:49

--- Camping Sol Luna/src/TipODePago.cpp
+++ Camping Sol Luna/src/TipODePago.cpp
@@ -7,28 +7,131 @@ using namespace std;
 #include <cstring>
 
 
-void TipoDePago::Cargar(){
+void TipoDePago::Cargar()
+{
 
     bool sectordisponible = true;
     system("cls");
+
     InfoCliente.Cargar();
     cout<<"Ingrese el ID Servicio(1- Carpa 2- Cabaña): ";
     cin>>IDServicio;
     cout<<endl;
-    do{
-    cout<<"Ingrese el Sector correcpondiente (A-C): ";
-    cin>>IDSertor;
-    if((strcmp(IDSector,"a")!=0) || (strcmp(IDSector,"A")!=0) || (strcmp(IDSector,"b")!=0) || (strcmp(IDSector,"B")!=0) || (strcmp(IDSector,"c")!=0) || (strcmp(IDSector,"C")!=0){
-       cout<<"Opcion Incorrecta, ingrese nuevamente una opcion valida"<<endl;
+    do
+    {
+        cout<<"Ingrese el Sector correcpondiente (A-C): ";
+        cin>>IDSertor;
+
+        if((strcmp(IDSector,"a")!=0) || (strcmp(IDSector,"A")!=0) || (strcmp(IDSector,"b")!=0) || (strcmp(IDSector,"B")!=0) || (strcmp(IDSector,"c")!=0) || (strcmp(IDSector,"C")!=0){
+        cout<<"Opcion Incorrecta, ingrese nuevamente una opcion valida"<<endl;
+        system("pause");
+        }
+
+        else
+        {
+            sectordisponible = false;
+        }
+
+    }while(sectordisponible);///esto verifica que el sector ingresado sea el correcto de la letra "a" a la "c" verifica que sea tanto mayuscula como minuscula habilitados para poner un sector valido.
+
+            cout<<" Medio a pagar: ";
+            cin>>ModoPago;
+            cout<<endl;
+            FechaPago.Cargar();
+            cout<<"Ingrese el Monto: ";
+            cin>>Monto;
+            cout<<endl;
+            Estado = 1; ///reservado- Hay que ver como modificar de reservado a pagado si se pago la totalidad. En caso de pagar la totalidad, se debe poner 0 u otro numero, y en caso de cancelado, valor 2.
+    }
+}
+
+void TipoDePago::Mostrar(){
+
+        system("cls");
+        InfoCliente.Mostrar();
+        cout<<"Servicio: "<<IDServicio<<endl;
+        cout<<"Sector asignado: "<<IDSector<<endl;
+        cout<<"Estado de Reserva: "<<Estado<<endl;///informar el estado en palabras no en numeros.
+       cout<<"Modo de Pago: "ModoPago<<endl;
+       cout<<" Fecha de Pago"<<endl;
+       cout<<FechaPago<<endl;
+       cout<<"Monto abonado: "<<Monto<<endl;
        system("pause");
+}
 
-       else{sectordisponible = false;}
+void TipoDePago::setCliente(Cliente datos){
 
-       }while(sectordisponible);///esto verifica que el sector ingresado sea el correcto de la letra "a" a la "c" verifica que sea tanto mayuscula como minuscula habilitados para poner un sector valido.
+    InfoCliente = datos;
 
-    FechaPago.Cargar();
-    cout<<"Ingrese el Monto: ";
-    cin>>Monto;
-    cout<<endl;
-    Estado = 1; ///reservado- Hay que ver como modificar de reservado a pagado si se pago la totalidad. En caso de pagar la totalidad, se debe poner 0 u otro numero, y en caso de cancelado, valor 2.
+}
+
+void TipoDePago::setIDServicio(int idservicio){
+
+    IDServicio=idservicio;
+}
+
+void TipoDePago::setIDSector(const char * idsector){
+
+    strcpy(IDSector,idsector);
+
+}
+
+void TipoDePago::setModoPago(const char *modopago){
+
+    strcpy(ModoPago,modopago);
+}
+
+void TipoDePago::setFechaPago(Fecha fechaopago){
+
+    FechaPago = fechapago;
+
+}
+
+void TipoDePago::setMonto(float monto){
+
+    Monto = monto;
+}
+
+void TipoDePago::setEstado(bool estado){
+
+    Estado = estado;
+
+}
+
+Cliente TipoDePago::getCliente(){
+
+    return InfoClientes;
+
+}
+
+int TipoDePago::getIDServicio(){
+
+    return IDServicio;
+}
+
+char TipoDePago::getIDSector(){
+
+    return IDSector;
+}
+
+char TipoDePago::getModoPago(){
+
+    return ModoPago;
+}
+
+Fecha TipoDePago::getFechaPago(){
+
+    return FechaPago;
+}
+
+float TipoDePago::getMonto(){
+
+    return Monto;
+
+}
+
+bool TipoDePago::getEstado(){
+
+    return Estado;
+
 }
