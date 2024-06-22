1a4aa2b1c2efc8ea320d6252435e3ee1656c5987
fercho-099 <69776793+fercho-099@users.noreply.github.com>
2024-06-22 11:25:50 -0300

    22

--- Camping Sol Luna/include/ArchivoReservas.h
+++ Camping Sol Luna/include/ArchivoReservas.h
@@ -13,24 +13,20 @@ public:
     void setAperturaArchivo(const char *n);///se usa para bak up
 
 	bool GrabarRegistro(Reservas obj);///se usa para carga normal
-	///bool GrabarRegistro(Reservas obj, int TotalRegistros);
+	///bool GrabarRegistro(Reservas obj, int TotalRegistros);///No funciona grabar todo junto
 	bool grabarRegistros(Reservas obj, int pos);///se usa en el metodo de modificar reserva ya que recibe como parametro un registro completo de reservas y la posicion donde se va a reescribir los datos modificados en el archivo
     void grabarRegistroNuevo(Reservas obj);///Sirve para grabar en archivo nuevo con una apertura wb y pisando el archivo viejo.
 
-    ///Reservas LeerRegistro(int pos);///se usa
+    ///Reservas LeerRegistro(int pos);
 	void LeerRegistrosTotales(Reservas &, int );///se usa para cargar el vector de objetos en su totalidad
     Reservas LeerRegistrosDinamicos(Reservas *DatoReserva, int pos);///Recibe el vector de registros cargados y la posiccion donde se encuentra en el registro y devuelve ese registro especifico.
 
-	int BuscarRegistro(int dni);/// Se Usa
+	int BuscarRegistro(int dni);/// Se Debe usar para buscar en el registro cliente si existe el cliente antes de cargarlo nuevamente
     int BuscarRegistro(Reservas *DatoReserva, int TotalRegistros, int dni);///se usa para buscar registro en la posicion y devuelve suposicion en el archivo
 
 	int contarRegistros();///se usa para memoria dinamica
 
-
-	bool ModificarRegistros(Reservas &obj, int pos);
-	void ModificarRegistros(Reservas *obj);///Recibe el objeto dinamico de reservas con informacion y se modifica.
-
-
+    void ModificarRegistros(Reservas *obj);///Recibe el objeto dinamico de reservas con informacion y se modifica.
 
     bool CrearBackUpManual();///Devuelve true o false si se pudo crear un back up correctamente de manera automatica.
 	void ListarRegistros();///Lista la totalidad de registro de un archivo especifico, puede ser el back up o el archivo actual.
